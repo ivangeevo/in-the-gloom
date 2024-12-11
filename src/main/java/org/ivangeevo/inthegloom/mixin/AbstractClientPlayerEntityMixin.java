ad1b07065b534b62cb725138a43e90d54ba1b4b5
Ivan Geevo <dbowgaming@gmail.com>
2024-12-11 14:11:18 +0200

    improved FOV logic code injection

--- src/main/java/org/ivangeevo/inthegloom/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/org/ivangeevo/inthegloom/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,5 +1,6 @@
 package org.ivangeevo.inthegloom.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
@@ -15,7 +16,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity implements GloomEffectsConstants
@@ -27,58 +30,35 @@ public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity imple
         super(world, pos, yaw, gameProfile);
     }
 
-    // TODO: Not sure if this is injecting in the proper place. Possibly could be changed for a better modification.
     @Inject(method = "getFovMultiplier", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;getActiveItem()Lnet/minecraft/item/ItemStack;"), cancellable = true)
-    private void injectedGloomFOV(CallbackInfoReturnable<Float> cir)
-    {
-        float f = 1.0f;
-        if (this.getAbilities().flying) {
-            f *= 1.1f;
-        }
-        if (this.getAbilities().getWalkSpeed() == 0.0f || Float.isNaN(f *= ((float)this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED) / this.getAbilities().getWalkSpeed() + 1.0f) / 2.0f) || Float.isInfinite(f)) {
-            f = 1.0f;
-        }
-
-        // INJECTED:
+                    target = "Lnet/minecraft/entity/player/PlayerAbilities;getWalkSpeed()F",
+                    shift = At.Shift.AFTER),
+            slice = @Slice(from = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/entity/player/PlayerAbilities;getWalkSpeed()F", ordinal = 1),
+                    to = @At(value = "INVOKE",
+                            target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;getActiveItem()Lnet/minecraft/item/ItemStack;")),
+            cancellable = true
+    )
+    private void injectBetween(CallbackInfoReturnable<Float> cir, @Local float f) {
         f *= updateGloomFOVMultiplier();
-        // END INJECTED
-
-        ItemStack itemStack = this.getActiveItem();
-        if (this.isUsingItem()) {
-            if (itemStack.isOf(Items.BOW)) {
-                int i = this.getItemUseTime();
-                float g = (float)i / 20.0f;
-                g = g > 1.0f ? 1.0f : (g *= g);
-                f *= 1.0f - g * 0.15f;
-            } else if (MinecraftClient.getInstance().options.getPerspective().isFirstPerson() && this.isUsingSpyglass()) {
-                cir.setReturnValue( 0.1f );
-            }
-        }
-
-        cir.setReturnValue( MathHelper.lerp(MinecraftClient.getInstance().options.getFovEffectScale().getValue().floatValue(), 1.0f, f) );
+        cir.setReturnValue(f);
     }
 
     @Unique
-    private float updateGloomFOVMultiplier()
-    {
+    private float updateGloomFOVMultiplier() {
         int iGloomLevel = getGloomLevel();
 
-        if ( iGloomLevel == 0 )
-        {
+        if ( iGloomLevel == 0 ) {
             currentGloomFOVMultiplier -= GLOOM_FOV_MULTIPLIER_DELTA_OUT_PER_TICK;
 
-            if (currentGloomFOVMultiplier < 1F )
-            {
+            if (currentGloomFOVMultiplier < 1F ) {
                 currentGloomFOVMultiplier = 1F;
             }
-        }
-        else
-        {
+
+        } else {
             currentGloomFOVMultiplier += GLOOM_FOV_MULTIPLIER_DELTA_IN_PER_TICK;
 
-            if (currentGloomFOVMultiplier > MAXIMUM_GLOOM_FOV_MULTIPLIER)
-            {
+            if (currentGloomFOVMultiplier > MAXIMUM_GLOOM_FOV_MULTIPLIER) {
                 currentGloomFOVMultiplier = MAXIMUM_GLOOM_FOV_MULTIPLIER;
             }
         }
