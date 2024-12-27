96cf4293e639eeef5e36fc5fd27c29f9dd1c82d0
Ivan Geevo <dbowgaming@gmail.com>
2024-12-27 13:43:59 +0200

    bump small changes

--- src/main/java/org/ivangeevo/inthegloom/config/SettingsGUI.java
+++ src/main/java/org/ivangeevo/inthegloom/config/SettingsGUI.java
@@ -3,14 +3,9 @@ package org.ivangeevo.inthegloom.config;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.StringVisitable;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import org.ivangeevo.inthegloom.InTheGloomMod;
-
-import java.util.List;
 
 public class SettingsGUI
 {
@@ -33,10 +28,10 @@ public class SettingsGUI
                 .build());
 
         general.addEntry(entryBuilder
-                .startBooleanToggle(Text.translatable("config.in_the_gloom.nether_gloom"), settingsCommon.netherGloom)
+                .startBooleanToggle(Text.translatable("config.in_the_gloom.the_nether_gloom"), settingsCommon.theNetherGloom)
                 .setDefaultValue(true)
-                .setSaveConsumer(newValue -> settingsCommon.netherGloom = newValue)
-                .setTooltip(Text.translatable("config.in_the_gloom.tooltip.nether_gloom"))
+                .setSaveConsumer(newValue -> settingsCommon.theNetherGloom = newValue)
+                .setTooltip(Text.translatable("config.in_the_gloom.tooltip.the_nether_gloom"))
                 .build());
 
         general.addEntry(entryBuilder
@@ -49,22 +44,5 @@ public class SettingsGUI
 
         return builder.build();
     }
-
-
-
-
-    public static Text[] wrapLines(Text text){
-        List<StringVisitable> lines = MinecraftClient.getInstance().textRenderer.getTextHandler()
-                .wrapLines(text,Math.max(MinecraftClient.getInstance().getWindow().getScaledWidth()/2 - 43,170),
-                        Style.EMPTY);
-        lines.get(0).getString();
-        Text[] textLines = new Text[lines.size()];
-        for (int i = 0; i < lines.size(); i++) {
-            textLines[i]=Text.literal(lines.get(i).getString());
-        }
-        return textLines;
-    }
-
-
 
 }
